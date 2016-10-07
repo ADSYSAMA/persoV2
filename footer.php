53a60d2ac6eac2ef541043e3142a9d7a8378eeae
ADSYSAMA <adsysama@gmail.com>
2016-10-07 14:24:49 +0200

    push

--- footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="row col-md-12" style="text-align: center;">
-      <hr style="width:100%; color:#B2C7FF; background-color:#777; height:3px; margin-top:20px;"/> 
-        <p style="margin-top:20px;"> <a href="https://www.facebook.com/adrien.simon06"><img src="images/svg/social-1_logo-facebook.svg"></a>  
-            <a href="https://www.linkedin.com/in/adrien-simon-1b556b110?"><img src="images/svg/social-1_logo-linkedin.svg"></a>  
-            <a href="https://twitter.com/ADSY_SAMA"><img src="images/svg/social-1_square-twitter.svg"></a> </p>
-      </footer>
--- index.php
+++ index.php
@@ -9,37 +9,28 @@
     <link href="style.css" rel="stylesheet">
   </head>
 
-  <body>
-  <?php require('menu.php'); ?>
+<body>
 
-    <div class="container">
-
-      <div class="row" style="margin-top: -20px;">
-        <div class="col-md-12">
-          <div class="row"  style="margin-top: 20px;">  
-            <section id="resume" class="col-md-8">
-              <h4><span class="glyphicon glyphicon-tasks"></span><b> RESUME</b></h4>
-              <img src="images/profil.png" style="float: left; height:100px; width:100px; margin-right: 10px; margin-bottom:auto; border-radius: 25px; margin:10px;">
-              <p style="font-size:13px;">Lorem ipsum dolor sit amet, consectetur <b>adipiscing elit</b>. Fusce id ligula ac metus tempor efficitur. Integer dapibus justo rhoncus nunc rutrum congue. Suspendisse gravida quam vel aliquet iaculis. Donec sed nulla eget ante tempus venenatis mattis et tortor. Maecenas vulputate erat magna, in porta mauris porta sed. Quisque ipsum purus, vulputate eget pharetra nec, vestibulum at nibh. Nunc congue varius arcu ac elementum.<b> Aliquam vulputate risus nulla, id cursus metus volutpat eu. Ut vitae magna scelerisque, ullamcorper eros ac, scelerisque neque.</b> Ut eget lorem eu odio sodales bibendum et id ex. Quisque luctus, odio ac lacinia convallis, elit libero tempus est, nec fermentum diam odio quis tortor. Praesent sit amet risus vel massa posuere faucibus at at ex. <b>Integer ultricies ut neque eu ultrices.</b> </p> 
-              
-            </section>
-
-            <article class="col-md-4">
-              <h4><span class="glyphicon glyphicon-info-sign"></span><b> NEWS</b></h4>
-              <p>Ut eget lorem eu odio sodales bibendum et id ex. Quisque luctus, odio ac lacinia convallis, elit libero tempus est, nec fermentum diam odio quis tortor. Praesent sit amet risus vel massa posuere faucibus at at ex. <b>Integer ultricies ut neque eu ultrices.</p>
-            </article>
-        </div>
-      </div>
-      <p class="row" style="text-align: center; margin-top:50px; margin-bottom:30px;"><a href="pro.php">My work » </a><a href="hobbys.php" style="margin-left:20px;">My hobbys » </a><a href="contact.php" style="margin-left:20px;">Contact me » </a></p>
+  <div class="background">
+    <img style="margin-left:570px; margin-top:180px;" src="images/circle.png"/>
+    <h1 class="name">ADSY SAMA</h1>
+    <h2 class="info">STUDENT BASED IN PARIS</h2>
   </div>
-  <?php require('footer.php'); ?>
-</div>
+
+  <div class="social"> 
+    <a href="https://www.facebook.com/adrien.simon06"><img src="images/svg/social-1_logo-facebook.svg"></a>
+    <a href="https://www.linkedin.com/in/adrien-simon-1b556b110?"><img src="images/svg/social-1_logo-linkedin.svg"></a>  
+    <a href="https://twitter.com/ADSY_SAMA"><img src="images/svg/social-1_square-twitter.svg"></a> 
+  </div>
+
+    <a class="mail" href="mailto:adrien.simon@edu.esiee.fr">adrien.simon@edu.esiee.fr</a>
+
+  
+    
 </body>
   <script src="bootstrap/js/jquery.js"></script>
   <script src="bootstrap/js/bootstrap.min.js"></script>
   <script>
-    $(function () {
-      $('.carousel').carousel({ interval: 2000 });
-    });
+    
   </script>
 </html>
